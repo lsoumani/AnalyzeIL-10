a9b4dd99061a5fd650358c1ee41dc2d16958e75e
lsoumani <56736900+lsoumani@users.noreply.github.com>
2020-05-05 15:42:45 -0400

    Update AnalyzeBlood.R

--- script/AnalyzeBlood.R
+++ script/AnalyzeBlood.R
@@ -84,3 +84,48 @@ IBS$IL10_result[IBS$IL10 < 0.25] <- "LOW"
 
 write.csv(IBS, "data_output/IL10_result.csv")
 
+
+## BTEC395 F2020 
+Project by loic soumani
+
+
+setwd("./")
+
+## Install necessary packages
+install.packages("ggplot2")
+library(ggplot2)
+
+## Read data
+IBS <- read.csv("../data/GSE124549_20200330.csv", header = TRUE)
+
+head(IBS)
+
+
+
+## Recursive analysis for regression  - RNA Expression
+## https://stackoverflow.com/questions/42464767/how-to-run-lm-regression-for-every-column-in-r
+## https://stackoverflow.com/questions/44170937/performing-lm-and-segmented-on-multiple-columns-in-r
+## http://www.learnbymarketing.com/tutorials/explaining-the-lm-summary-in-r/
+## https://tutorials.iq.harvard.edu/R/Rstatistics/Rstatistics.html
+
+view(IBS)
+## Access only the columns with RNA Expression
+names(IBS)[37:286]
+
+##  Make a data frame of list type
+storage <- list()
+
+## linear regression for each expressed gene
+for(i in names(IBS)[37:286]){
+  storage[[i]]  <- lm(get(i) ~ serum.IL10, IBS)
+}
+
+summary(storage$AGO2)
+summary(storage$AGO2)$r.squared
+summary(storage$AGO2)$coefficients[,4]
+
+## output the results of the 250 genes in data_output folder
+sink('../data_output/serumIL10_storage.txt', append = TRUE)
+print(storage)
+sink()
+
